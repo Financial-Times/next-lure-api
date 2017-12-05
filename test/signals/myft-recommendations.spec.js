44a4e63f81bfdef3bc435adc4301b56097e500f4
fenglish <asuka.ochi@ft.com>
2017-12-05 17:04:21 +0000

    comment out a WIP test  🐿 v2.5.16

--- test/signals/myft-recommendations.spec.js
+++ test/signals/myft-recommendations.spec.js
@@ -1,18 +1,18 @@
-const { expect } = require('chai');
-const subject = require('../../server/signals/myft-recommendations');
-let params;
-
-describe('myFT Recommendations', () => {
-
-	beforeEach(() => {
-		params = { locals: { slots: { ribbon: true }, userId:'', q1Length: 5 }};
-	});
-
-	// it('should return correct response', () => {
-	// 	return subject({}, params)
-	// 		.then(result => {
-	// 			expect(result).to.eql({ data : { popularConcepts : [], user: { followed: [] } }});
-	// 		})
-	// });
-
-});
+// const { expect } = require('chai');
+// const subject = require('../../server/signals/myft-recommendations');
+// let params;
+//
+// describe('myFT Recommendations', () => {
+//
+// 	beforeEach(() => {
+// 		params = { locals: { slots: { ribbon: true }, userId:'', q1Length: 5 }};
+// 	});
+//
+// 	it('should return correct response', () => {
+// 		return subject({}, params)
+// 			.then(result => {
+// 				expect(result).to.eql({ data : { popularConcepts : [], user: { followed: [] } }});
+// 			})
+// 	});
+//
+// });
