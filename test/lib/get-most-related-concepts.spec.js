78081e2218125a05574b0ca7abbab4819cd1a0a5
kavanagh <kavanagh@users.noreply.github.com>
2021-04-16 11:49:25 +0100

    Tweak fallback logic for most related concepts
    It now uses the implicitlyAbout predicate as the main fallback
    for most scenarios. But also keeps the original brand fallback
     in case that was important for some edge case but without
    using next-elastic's deprecated brandConcept property.

--- test/lib/get-most-related-concepts.spec.js
+++ test/lib/get-most-related-concepts.spec.js
@@ -2,41 +2,62 @@ const {expect} = require('chai');
 const subject = require('../../server/lib/get-most-related-concepts');
 
 describe('get most related concepts', () => {
-	it('use about and isPrimarilyClassifiedBy by default', () => {
+	it('use about, isPrimarilyClassifiedBy and implicitlyAbout by default', () => {
 		const result = subject({
-			annotations: [{
-				predicate: 'http://www.ft.com/ontology/annotation/about',
-				id: 0
-			}, {
-				predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
-				id: 1
-			}]
+			annotations: [
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/product/Brand',
+					id: 3,
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/implicitlyAbout',
+					id: 2,
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
+					id: 1,
+				},{
+					predicate: 'http://www.ft.com/ontology/annotation/about',
+					id: 0,
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				}
+			]
 		});
 		expect(result[0].id).to.equal(0);
 		expect(result[1].id).to.equal(1);
-	});
-
-	it('fallback to brand if either preferred annotation is missing', () => {
-		const result = subject({
-			annotations: [{
-				predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
-				id: 1
-			}],
-			brandConcept: {
-				id: 2,
-				predicate: 'whatevs'
-			}
-		});
-		expect(result[0].id).to.equal(1);
-		expect(result[1].id).to.equal(2);
+		expect(result[2].id).to.equal(2);
+		expect(result[3].id).to.equal(3);
+		expect(result.length).to.equal(4);
 	});
 
 	it('handle case where only one preferred annotation is present', () => {
 		const result = subject({
-			annotations: [{
-				predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
-				id: 1
-			}]
+			annotations: [
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
+					id: 1
+				}
+			]
 		});
 		expect(result[0].id).to.equal(1);
 		expect(result.length).to.equal(1);
@@ -44,24 +65,27 @@ describe('get most related concepts', () => {
 
 	it('handle case where no preferred annotations are present', () => {
 		const result = subject({
-			annotations: []
+			annotations: [
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				},
+				{
+					predicate: 'http://www.ft.com/ontology/classification/isClassifiedBy',
+					directType: 'http://www.ft.com/ontology/Topic',
+					id: 'should-be-ignored',
+				},
+			]
 		});
 		expect(result).to.be.undefined;
 	});
 
-	it('handle case where no about, and isPrimarilyClassifiedBy by the brand', () => {
+	it('handle case where no annotations are present', () => {
 		const result = subject({
-			annotations: [{
-				predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
-				id: 1
-			}],
-			brandConcept: {
-				predicate: 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy',
-				id: 1
-			}
+			annotations: []
 		});
-		expect(result[0].id).to.equal(1);
-		expect(result.length).to.equal(1);
+		expect(result).to.be.undefined;
 	});
 
 	it('handle case where content has no annotations', () => {
