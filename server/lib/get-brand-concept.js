5c302331a3fa2352bcc10be058d44b3e3f6bf71d
kavanagh <kavanagh@users.noreply.github.com>
2021-05-14 16:34:30 +0100

    Choose the brand by ranking them
    This settles the choice when there are mutiple competing
    important brands to use. It implements an algorithm to rank the
    brand annotations depending on the predicate used
    (isClassifiedBy or implicityClassifiedBy) and a hard-coded priority
    list.
    
    Here's an example of when this is particularly useful. An item of
    content is annotated with the following:
      * isClassifiedBy Brand "Big Read"
      * isImplicitlyClassifiedBy Brand "Moral Money"
      * isImplicitlyClassifiedBy Brand "Climate Capital"

--- server/lib/get-brand-concept.js
+++ server/lib/get-brand-concept.js
@@ -1,11 +1,42 @@
-module.exports = content => {
+const conceptIds = require('@financial-times/n-concept-ids');
+const { Predicate, ConceptType, withType } = require('./content');
+const scoredSort = require('./scored-sort');
+
+module.exports = sortAnnotations;
+
+const priorityList = new Map([
+	// Brands that Editorially important and have a high likelyhood of being
+	// in competition with other brands when scoring.
+	[conceptIds.brand.climateCapital, 2],
+
+	// other weighted brands
+	[conceptIds.brand.etfHub, 1],
+	[conceptIds.brand.htsi, 1],
+	[conceptIds.brand.magazine, 1],
+	[conceptIds.brand.globetrotter, 1],
+	[conceptIds.brand.moralMoney],
+
+	// Weighted Down:
+	// The Special Reports brand has many sub-brands. When one of the sub-brands
+	// is in use we'd favour it over the broader/generic Special Reports brand
+	[conceptIds.brand.specialReport, -1],
+]);
+
+const sortBrandsByScore = scoredSort([
+	({predicate}) => Predicate.isClassifiedBy === predicate && 1.5,
+	({id}) => priorityList.has(id) && (priorityList.get(id) || 0) + 1,
+]);
+
+function sortAnnotations (content) {
 	if (!content.annotations) {
 		return undefined;
 	}
 
-	const brandConcept = content.annotations.find(annotation => annotation.directType === 'http://www.ft.com/ontology/product/Brand');
+	const brands = content.annotations.filter(withType(ConceptType.Brand));
 
-	// ToDo: Some kind of predicate sorting.
+	if (brands.length <= 1) {
+		return brands[0];
+	}
 
-	return brandConcept || undefined;
-};
+	return sortBrandsByScore(brands)[0];
+}
