ca41229edbd8cd42e4cf73ae89a4bd0226892a44
kavanagh <kavanagh@users.noreply.github.com>
2021-05-14 16:35:03 +0100

    Refactor logic for choosing the most related topic
    Reuses scoring/ranking approach from get-brand-concept.js
    
    It is now opinionated about the ordering of concept types. Previously, two
    annotations with the same predicate would be sorted randomly depending
    on the order they appeared in the API respinse from ElasrticSearch. Now
    the order is deterministic.

--- server/lib/get-most-related-concepts.js
+++ server/lib/get-most-related-concepts.js
@@ -1,21 +1,53 @@
-module.exports = content => {
+const { Predicate, ConceptType } = require('./content');
+const scoredSort = require('./scored-sort');
+
+module.exports = sortAnnotations;
+
+const rankedPredicates = rank(
+	Predicate.about,
+	Predicate.isPrimarilyClassifiedBy,
+	Predicate.implicitlyAbout,
+	Predicate.isClassifiedBy,
+);
+
+const rankedTypes = rank(
+	ConceptType.Organisation,
+	ConceptType.Location,
+	ConceptType.Topic,
+	ConceptType.Brand,
+);
+
+const sortAnnotationsByScore = scoredSort([
+	({predicate}) => Math.pow(rankedPredicates[predicate], 2),
+	({directType}) => rankedTypes[directType],
+], 2);
+
+function sortAnnotations (content) {
 	if (!content.annotations) {
 		return undefined;
 	}
 
+	const sortedAnnotations = sortAnnotationsByScore(content.annotations);
+
 	const displayTag = content.displayConcept && content.displayConcept.isDisplayTag ? content.displayConcept : null;
 
-	const mostRelatedConcepts = [
-		displayTag,
-		content.annotations.find(annotation => annotation.predicate === 'http://www.ft.com/ontology/annotation/about'),
-		content.annotations.find(annotation => annotation.predicate === 'http://www.ft.com/ontology/classification/isPrimarilyClassifiedBy'),
-		content.annotations.find(annotation => annotation.predicate === 'http://www.ft.com/ontology/implicitlyAbout'),
-		content.annotations.find(
-			annotation => annotation.predicate === 'http://www.ft.com/ontology/classification/isClassifiedBy'
-			&& annotation.directType === 'http://www.ft.com/ontology/product/Brand'
-		)
-	]
-		.filter(concept => !!concept);
+	if(displayTag) {
+		sortedAnnotations.unshift(displayTag);
+	}
 
-	return mostRelatedConcepts.length ? mostRelatedConcepts : undefined;
-};
+	return sortedAnnotations.length ? sortedAnnotations : undefined;
+}
+
+// Create an object with a rank score for each element
+// The first in the list has the highest score
+// e.g rank('a', 'b', 'c', 'd') === {a: 4, b: 3, c: 2, d: 1}
+function rank (...strings) {
+	return strings
+		// make a copy because .reverse() mutates the original array
+		.slice(0)
+		.reverse()
+		.reduce((o, k, i) => {
+			o[k] = (i + 1);
+			return o;
+		}, {});
+}
