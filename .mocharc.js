cf53e9e9ba305cf87a59cbb53b4f9d6c8e7693dd
Asuka Ochi <asuka.ochi@ft.com>
2024-01-12 11:44:26 +0000

    Set the same env var as the Makefile

--- .mocharc.js
+++ .mocharc.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+process.env.AWS_SIGNED_FETCH_DISABLE_DNS_RESOLUTION = 'true';
 process.env.FT_GRAPHITE_KEY = 'dummy-graphite-key';
 process.env.INTERNAL_SMOKE_TEST_KEY = 'dummy-smoke-test-key';
 
